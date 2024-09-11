d0a15003eba84eed1647d15865c6048d3da8f8ee
suchithrauk <84430355+suchithrauk@users.noreply.github.com>
2024-09-11 16:39:43 +0530

    Add files via upload
    Day1

---
+++ Day1/FirstProgram.java
@@ -0,0 +1,17 @@
+package Week1.Day1;
+
+//Access Modifier     //Keyword      //classname
+public                class          FirstProgram {
+	
+	//create a main class
+		
+	//access modifier   //keyword   //return type    //method name
+	public               static     void              main(String args[]) {
+		
+		
+		System.out.println("First Program");
+			
+			
+		}
+
+}
---
+++ Day1/SecondProgram.java
@@ -0,0 +1,10 @@
+package Week1.Day1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("second program");
+	}
+	
+}
